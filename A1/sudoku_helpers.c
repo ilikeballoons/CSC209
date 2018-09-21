2c56d8ace28a348b154181d0427a4b1f840af940
Markus <markus@markus.com>
2018-09-21 10:56:19 -0400

    Update files

---
+++ A1/sudoku_helpers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Each of the n elements of array elements, is the address of an
+ * array of n integers.
+ * Return 0 if every integer is between 1 and n^2 and all
+ * n^2 integers are unique, otherwise return 1.
+ */
+int check_group(int **elements, int n) {
+    // TODO: replace this return statement with a real function body
+    return 0;
+}
+
+/* puzzle is a 9x9 sudoku, represented as a 1D array of 9 pointers
+ * each of which points to a 1D array of 9 integers.
+ * Return 0 if puzzle is a valid sudoku and 1 otherwise. You must
+ * only use check_group to determine this by calling it on
+ * each row, each column and each of the 9 inner 3x3 squares
+ */
+int check_regular_sudoku(int **puzzle) {
+
+    // TODO: replace this return statement with a real function body
+    return 0;
+}
