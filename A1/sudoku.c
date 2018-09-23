d587f817af9beae8a663df137c2ec903dcc1e7d0
Denny Wiseman <d.wiseman@utoronto.ca>
2018-09-23 16:31:30 -0400

    completed sudoku algorithm

--- A1/sudoku.c
+++ A1/sudoku.c
@@ -3,9 +3,10 @@
 int check_regular_sudoku(int **puzzle);
 
 int main() {
-    // You could change these values to hand-test your function. 
+    // You could change these values to hand-test your function.
     // This example is invalid. Notice that the very middle inner 3x3
     // box has a pair of 3's and a pair of 4's, no 6 and no 7.
+
     int row1[9] = {1, 2, 3, 4, 5, 6, 7, 8 ,9};
     int row2[9] = {4, 5, 6, 7, 8 ,9, 1, 2, 3};
     int row3[9] = {7, 8 ,9, 1, 2, 3, 4, 5, 6};
@@ -15,6 +16,17 @@ int main() {
     int row7[9] = {3, 4, 5, 6, 7, 8 ,9, 1, 2};
     int row8[9] = {6, 7, 8 ,9, 1, 2, 3, 4, 5};
     int row9[9] = {2, 3, 4, 5, 6, 7, 8, 9, 1};
+
+    // // This puzzle is correct
+    // int row1[9] = {2, 9, 5, 7, 4, 3, 8, 6, 1};
+    // int row2[9] = {4, 3, 1, 8, 6, 5, 9, 2, 7};
+    // int row3[9] = {8, 7, 6, 1, 9, 2, 5, 4, 3};
+    // int row4[9] = {3, 8, 7, 4, 5, 9, 2, 1, 6};
+    // int row5[9] = {6, 1, 2, 3, 8, 7, 4, 9, 5};
+    // int row6[9] = {5, 4, 9, 2, 1, 6, 7, 3, 8};
+    // int row7[9] = {7, 6, 3, 5, 3, 4, 1, 8, 9};
+    // int row8[9] = {9, 2, 8, 6, 7, 1, 3, 5, 4};
+    // int row9[9] = {1, 5, 4, 9, 3, 8, 6, 7, 2};
 
     int *puzzle[9] = {row1, row2, row3, row4, row5, row6, row7, row8, row9};
     if (check_regular_sudoku(puzzle)) {
--- A1/sudoku_helpers.c
+++ A1/sudoku_helpers.c
@@ -6,18 +6,119 @@
  * n^2 integers are unique, otherwise return 1.
  */
 int check_group(int **elements, int n) {
-    // TODO: replace this return statement with a real function body
-    return 0;
+   int is_all_unique[n*n];
+   for (int i = 0; i < n*n; i++) {
+     is_all_unique[i] = 0; // initialize the array with 0s
+   }
+
+   for (int i = 0; i < n; i++) {
+     //for each array
+     for (int j = 0; j < n; j++) {
+       //for each array element
+       int current_number = elements[i][j];
+       if (is_all_unique[(current_number - 1)] == 0) {
+         is_all_unique[(current_number - 1)] = 1;
+       }
+     }
+   }
+
+   for (int i = 0; i < (n*n); i++) {
+     if (is_all_unique[i] == 0) {
+       return 1;
+     }
+   }
+   return 0;
+}
+
+
+/*
+* This method takes a sudoku puzzle, which is a collection of rows,
+* and converts it into a collection of columns
+*/
+int *build_columns (int **puzzle) {
+  int col1[9] = {};
+  int col2[9] = {};
+  int col3[9] = {};
+  int col4[9] = {};
+  int col5[9] = {};
+  int col6[9] = {};
+  int col7[9] = {};
+  int col8[9] = {};
+  int col9[9] = {};
+
+  int *columns[9] = {col1, col2, col3, col4, col5, col6, col7, col8, col9};
+
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; i < 9; j++) {
+      // take the value of puzzle[j][i] and append it to columns[i][j]
+      columns[i][j] = puzzle[j][i];
+    }
+  }
+  return *columns;
+}
+
+/*
+* This method takes a collection of rows and converts it into a collection of boxes
+*/
+int *build_boxes (int **puzzle) {
+  int puzzle_1d[81];
+  int boxes_1d[81];
+  int counter = 0;
+
+  //convert 2d array into 1d array
+  for (int row = 0; row < 9; row++) {
+    for (int col = 0; col < 9; col++) {
+      puzzle_1d[col + row * 9] = puzzle[row][col];
+    }
+  }
+
+  //build the boxes into a 1d array
+  for (int i = 0; i < 3; i++) {
+    for (int j = i; j < i + 7; j += 3 ) {
+      for (int k = 3*j; k < j + 3; k++) {
+        boxes_1d[counter] = puzzle_1d[k];
+        counter++;
+      }
+    }
+  }
+
+  int a1[9], b1[9], c1[9], a2[9], b2[9], c2[9], a3[9], b3[9], c3[9];
+  int *boxes[9] = {a1, b1, c1, a2, b2, c2, a3, b3, c3};
+  counter = 0;
+
+  //convert the 1d array into a 2d array
+  for (int row = 0; row < 9; row++) {
+    for (int col = 0; col < 9; col++) {
+      boxes[row][col] = boxes_1d[counter];
+      counter++;
+    }
+  }
+
+  return *boxes;
 }
 
 /* puzzle is a 9x9 sudoku, represented as a 1D array of 9 pointers
- * each of which points to a 1D array of 9 integers.
- * Return 0 if puzzle is a valid sudoku and 1 otherwise. You must
- * only use check_group to determine this by calling it on
- * each row, each column and each of the 9 inner 3x3 squares
- */
+* each of which points to a 1D array of 9 integers.
+* Return 0 if puzzle is a valid sudoku and 1 otherwise. You must
+* only use check_group to determine this by calling it on
+* each row, each column and each of the 9 inner 3x3 squares
+*/
 int check_regular_sudoku(int **puzzle) {
+  int error_flag = 0;
+  error_flag = check_group(puzzle, 3);
+  if (error_flag == 1) {
+    return 1;
+  }
+  int *columns = build_columns(puzzle);
+  error_flag = check_group(&columns, 3);
+  if (error_flag == 1) {
+    return 1;
+  }
 
-    // TODO: replace this return statement with a real function body
-    return 0;
+  int *boxes = build_boxes(puzzle);
+  error_flag = check_group(&boxes, 3);
+  if (error_flag == 1) {
+    return 1;
+  }
+  return 0;
 }
