789e953ccf6c4b17f9440e792ec4b58626f687d7
Denny Wiseman <d.wiseman@utoronto.ca>
2018-09-25 19:58:33 -0400

    reads and outputs appropriately, need to tidy code and make sure it passes all use cases

--- A1/most_processes.c
+++ A1/most_processes.c
@@ -1,19 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 1024
+#define MAX_UID_SIZE 31
+
+void read_lines() {
+  int greatest_pid_num = 1;
+  char greatest_uid[MAX_UID_SIZE];
+
+  char current_uid[MAX_UID_SIZE];
+  int current_pid_num = 1;
+
+  char line_uid[MAX_UID_SIZE];
+  char input_line[MAX_LINE_SIZE];
+
+  //read first line and set the appropriate variables
+  fgets(input_line, MAX_LINE_SIZE, stdin);
+  sscanf(input_line, "%s", greatest_uid);
+
+  while(fgets(input_line, MAX_LINE_SIZE, stdin) != NULL) {
+    sscanf(input_line, "%s", line_uid);
+    //read uid == current_uid
+    if(strcmp(line_uid, current_uid) == 0) {
+      current_pid_num++;
+      if (greatest_pid_num < current_pid_num) {
+        greatest_pid_num = current_pid_num;
+        strcpy(greatest_uid, current_uid);
+      }
+    } else {
+      //read uid != current_uid
+      strcpy(current_uid, line_uid);
+      current_pid_num = 1;
+    }
+  }
+  printf("%s %d\n", greatest_uid, greatest_pid_num);
+}
 
 int main(int argc, char **argv) {
-    	int ppid = strtod(argv[1], NULL);
+  if(argc > 2) {
+    printf("USAGE: most_processes [ppid]\n");
+    return 1;
+  }
 
-	if(argc > 2) {
-		printf("USAGE: most_processes [ppid]\n");
-		return 1;
-	}
-
-	if(argc == 2) {
-		char *ptr;
-		int ppid = strtol(argv[1], &ptr, 10); //capture the ppid
-		printf("peepeeid %d", ppid);
-		return 0;
-	}
+  if(argc == 2) {
+    char *ptr;
+    int ppid = strtol(argv[1], &ptr, 10); //capture the ppid
+    read_lines();
+    return 0;
+  }
 
 }
