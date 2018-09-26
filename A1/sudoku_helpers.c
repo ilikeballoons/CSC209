65c69269523e549580c9027fbe85157e80e88032
Denny Wiseman <d.wiseman@utoronto.ca>
2018-09-25 20:46:00 -0400

    sudoku_helpers.c cleanup and comments

--- A1/sudoku_helpers.c
+++ A1/sudoku_helpers.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -32,10 +31,10 @@ int check_group(int **elements, int n) {
    return 0;
 }
 
-
 /*
-* This method takes a sudoku puzzle, which is a collection of rows,
-* and converts it into a collection of columns
+* puzzle is a pointer to an array of arrays which represents the rows of the puzzle
+* cols is 2d array representing the columns of the puzzle
+* This function converts the rows into columns and places the values in cols
 */
 void build_columns (int **puzzle, int cols[9][9]) {
   for (int row = 0; row < 9; row++) {
@@ -43,38 +42,39 @@ void build_columns (int **puzzle, int cols[9][9]) {
     // take the value of puzzle[j][i] and append it to columns[i][j]
     cols[row][col] = puzzle[col][row];
     }
-  };
+  }
  }
 
-/*
-* This method takes a collection of rows and converts it into a collection of boxes
-*/
+ /*
+ * puzzle is a pointer to an array of arrays which represents the rows of the puzzle
+ * boxes is 2d array representing the internal boxes of the puzzle
+ * This function converts the rows into boxes and places the values in boxes
+ */
 void build_boxes (int **puzzle, int boxes[9][9]) {
 int index = 0;
 
-for (int startingR=0;startingR<9;startingR+=3)
-{	
-for (int startingC =0;startingC<9;startingC+=3)
-	{
-		int z = 0;
-		for (int r =startingR;r<startingR+3;r++)
-		{
-			for (int c = startingC;c<startingC+3;c++)
-			{
-				boxes[index][z] = puzzle[r][c];
-				z++;
-			}
-		}
-		index++;
-	}
-}
+for (int starting_row = 0; starting_row < 9; starting_row += 3) {
+  for (int starting_col = 0; starting_col < 9; starting_col += 3) {
+  		int z = 0;
+  		for (int r = starting_row; r < starting_row + 3; r++) {
+  			for (int c = starting_col; c < starting_col + 3; c++) {
+  				boxes[index][z++] = puzzle[r][c];
+  			}
+  		}
+  		index++;
+  	}
+  }
 }
 
+/*
+* Takes a single 9 item array of the puzzle and converts it into a 3x3 array
+* as needed for the check_group function described in the assignment sheet
+*/
 void build_2d_array(int group[9], int** array_2d) {
   int counter = 0;
   for (int i = 0; i < 3; i++) {
-	for (int j = 0; j < 3; j++) {
-		array_2d[i][j] = group[counter++];
+  	for (int j = 0; j < 3; j++) {
+  		array_2d[i][j] = group[counter++];
 		}
 	}
 }
@@ -92,9 +92,31 @@ int check_regular_sudoku(int **puzzle) {
     array_2d[i] = malloc(3 * sizeof(int));
   }
 
+  // check 9 rows
+  for (int row = 0; row < 9; row++) {
+    build_2d_array(puzzle[row], array_2d);
+    error_flag = check_group(array_2d, 3);
+    if (error_flag == 1) {
+      return 1;
+     }
+   }
+
+  //build columns
+  int columns[9][9] = {};
+  build_columns(puzzle, columns);
+  //check 9 columns
+  for (int col = 0;  col < 9; col++) {
+    build_2d_array(columns[col], array_2d);
+    error_flag = check_group(array_2d, 3);
+    if (error_flag == 1) {
+      return 1;
+    }
+  }
+
+  //build boxes
   int boxes[9][9] = {};
   build_boxes(puzzle, boxes);
-
+  //check 9 boxes
   for (int box = 0; box < 9; box++) {
     build_2d_array(boxes[box], array_2d);
     error_flag = check_group(array_2d, 3);
@@ -103,23 +125,5 @@ int check_regular_sudoku(int **puzzle) {
     }
   }
 
-  for (int row = 0; row < 9; row++) {
-    //build the 2d array and pass it in to check_group
-    build_2d_array(puzzle[row], array_2d);
-    error_flag = check_group(array_2d, 3);
-    if (error_flag == 1) {
-      return 1;
-     }
-   }
-
-  int columns[9][9] = {};
-  build_columns(puzzle, columns);
-  for (int col = 0;  col < 9; col++) {
-    build_2d_array(columns[col], array_2d);
-    error_flag = check_group(array_2d, 3);
-    if (error_flag == 1) {
-      return 1;
-    }
-  }
-  return 0;
+  return 0; // if the program has reached here, then it is a valid sudoku
 }
