a6de573102af2df147f0dec1b3649a085c551f7e
Denny Wiseman <d.wiseman@utoronto.ca>
2018-09-25 20:27:47 -0400

    completed functionality

--- A1/most_processes.c
+++ A1/most_processes.c
@@ -9,15 +9,11 @@ void read_lines() {
   int greatest_pid_num = 1;
   char greatest_uid[MAX_UID_SIZE];
 
-  char current_uid[MAX_UID_SIZE];
   int current_pid_num = 1;
+  char current_uid[MAX_UID_SIZE];
 
   char line_uid[MAX_UID_SIZE];
   char input_line[MAX_LINE_SIZE];
-
-  //read first line and set the appropriate variables
-  fgets(input_line, MAX_LINE_SIZE, stdin);
-  sscanf(input_line, "%s", greatest_uid);
 
   while(fgets(input_line, MAX_LINE_SIZE, stdin) != NULL) {
     sscanf(input_line, "%s", line_uid);
@@ -37,17 +33,52 @@ void read_lines() {
   printf("%s %d\n", greatest_uid, greatest_pid_num);
 }
 
+void read_lines_ppid(int user_ppid) {
+  int greatest_pid_num = 1; //TODO: what if the ppid is not present?
+  char greatest_uid[MAX_UID_SIZE];
+  int current_pid_num = 1; //TODO: see line 41
+  char current_uid[MAX_UID_SIZE];
+  int line_ppid = -1;
+  char line_uid[MAX_UID_SIZE];
+
+  char input_line[MAX_LINE_SIZE];
+
+  while(fgets(input_line, MAX_LINE_SIZE, stdin) != NULL) {
+    sscanf(input_line, "%s %*d %d", line_uid, &line_ppid);
+    //ppid matches param
+    if(line_ppid == user_ppid) {
+      //read uid == current_uid
+      if(strcmp(line_uid, current_uid) == 0) {
+        current_pid_num++;
+        if(greatest_pid_num < current_pid_num) {
+          greatest_pid_num = current_pid_num;
+          strcpy(greatest_uid, current_uid);
+        }
+      } else {
+        //read uid != current uid
+        strcpy(current_uid, line_uid);
+        current_pid_num = 1;
+      }
+    }
+  }
+  printf("%s %d\n", greatest_uid, greatest_pid_num);
+}
+
 int main(int argc, char **argv) {
   if(argc > 2) {
     printf("USAGE: most_processes [ppid]\n");
     return 1;
   }
 
-  if(argc == 2) {
+  if(argc == 2) { //command line options included
     char *ptr;
     int ppid = strtol(argv[1], &ptr, 10); //capture the ppid
-    read_lines();
+    read_lines_ppid(ppid);
     return 0;
   }
 
+  if(argc == 1) { //no command line options
+    read_lines();
+    return 0;
+  }
 }
