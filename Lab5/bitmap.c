5b0d63d00fc00d478f37997264233a14b0621435
Denny Wiseman <d.wiseman@utoronto.ca>
2018-10-10 19:03:03 -0400

    completed read bitmap metadata

--- Lab5/bitmap.c
+++ Lab5/bitmap.c
@@ -4,26 +4,41 @@
 
 
 /*
- * Read in the location of the pixel array, the image width, and the image 
+ * Read in the location of the pixel array, the image width, and the image
  * height in the given bitmap file.
  */
 void read_bitmap_metadata(FILE *image, int *pixel_array_offset, int *width, int *height) {
+  int error;
 
+  fseek(image, 10, SEEK_SET);
+  error += fread(pixel_array_offset, sizeof(int), 1, image);
+
+  fseek(image, 18, SEEK_SET);
+  error += fread(width, sizeof(int), 1, image);
+
+  fseek(image, 22, SEEK_SET);
+  error += fread(height, sizeof(int), 1, image);
+
+  // if (error != 3) {
+  //   not sure if I should print this error message (might screw up autotest)
+  //   fprintf(stderr, "Error: incorrect number of elements read.");
+  //   exit(1);
+  // }
 }
 
 /*
  * Read in pixel array by following these instructions:
  *
- * 1. First, allocate space for m `struct pixel *` values, where m is the 
- *    height of the image.  Each pointer will eventually point to one row of 
+ * 1. First, allocate space for m `struct pixel *` values, where m is the
+ *    height of the image.  Each pointer will eventually point to one row of
  *    pixel data.
- * 2. For each pointer you just allocated, initialize it to point to 
+ * 2. For each pointer you just allocated, initialize it to point to
  *    heap-allocated space for an entire row of pixel data.
- * 3. Use the given file and pixel_array_offset to initialize the actual 
- *    struct pixel values. Assume that `sizeof(struct pixel) == 3`, which is 
+ * 3. Use the given file and pixel_array_offset to initialize the actual
+ *    struct pixel values. Assume that `sizeof(struct pixel) == 3`, which is
  *    consistent with the bitmap file format.
- *    NOTE: We've tested this assumption on the Teaching Lab machines, but 
- *    if you're trying to work on your own computer, we strongly recommend 
+ *    NOTE: We've tested this assumption on the Teaching Lab machines, but
+ *    if you're trying to work on your own computer, we strongly recommend
  *    checking this assumption!
  * 4. Return the address of the first `struct pixel *` you initialized.
  */
