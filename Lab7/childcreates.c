457232c8cba92dbcc587f9ff2d52a74030905f36
Denny Wiseman <d.wiseman@utoronto.ca>
2018-10-26 12:11:22 -0400

    completed childcreates

---
+++ Lab7/childcreates.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int Fork () {
+  int ret = fork();
+  if (ret < 0) {
+    perror("fork");
+    exit(1);
+  }
+  return ret;
+}
+
+int main(int argc, char **argv) {
+    int i;
+    int iterations;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: forkloop <iterations>\n");
+        exit(1);
+    }
+
+    iterations = strtol(argv[1], NULL, 10);
+    int n;
+    for (i = 0; i < iterations; i++) {
+      n = Fork();
+      printf("ppid = %d, pid = %d, i = %d\n", getppid(), getpid(), i);
+      if (n != 0) {
+        exit(0);
+      }
+    }
+
+    return 0;
+}
