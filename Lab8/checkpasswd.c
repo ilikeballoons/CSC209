7a13094a094c70088bdccd8171bdcd83b9366df2
Denny Wiseman <d.wiseman@utoronto.ca>
2018-11-01 20:39:23 -0400

    close

--- Lab8/checkpasswd.c
+++ Lab8/checkpasswd.c
@@ -7,10 +7,15 @@
 
 #define MAXLINE 256
 #define MAX_PASSWORD 10
+#define READ 0
+#define WRITE 1
 
 #define SUCCESS "Password verified\n"
 #define INVALID "Invalid password\n"
 #define NO_USER "No such user\n"
+
+#define VALIDATE_EXEC "./validate"
+#define VALIDATE "validate"
 
 int main(void) {
   char user_id[MAXLINE];
@@ -24,8 +29,49 @@ int main(void) {
       perror("fgets");
       exit(1);
   }
-  
 
+  int pfd[2];
+  pipe(pfd);
+  //todo: error check
 
+  write(pfd[WRITE], user_id, 32);
+  //check
+  write(pfd[WRITE], password, MAX_PASSWORD);
+  //check
+
+  int pid = fork();
+  //fork
+  if (pid < 0) {
+    perror("fork");
+    exit(1);
+  }
+
+  if (pid > 0) { //parent
+    close(pfd[WRITE]);
+    close(pfd[READ]);
+    int status;
+    if(wait(&status) == -1) {
+      perror("wait");
+      exit(1);
+    }
+    if (WIFEXITED(status)) {
+      int result = WEXITSTATUS(status);
+      if(result == 0) {
+        printf(SUCCESS);
+      } else if(result == 2) {
+        printf(INVALID);
+      } else if(result == 3) {
+        printf(NO_USER);
+      }
+    }
+  //parent:
+  //close read pipe
+  //wait for chidl to return then print one of the constants based on the return value of validate
+} else if (pid == 0) {
+    close(pfd[WRITE]);
+    dup2(STDIN_FILENO, pfd[READ]);
+    execl(VALIDATE_EXEC, VALIDATE, NULL);
+    close(pfd[READ]);
+  }
   return 0;
 }
