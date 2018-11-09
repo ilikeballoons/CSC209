bfd809a2f1a5edf464183f6fff1e39273f6202b1
Denny Wiseman <d.wiseman@utoronto.ca>
2018-11-09 15:43:32 -0500

    completed task 2

--- A3/worker.c
+++ A3/worker.c
@@ -7,7 +7,43 @@
 #include "freq_list.h"
 #include "worker.h"
 
-void *Malloc (int size) {
+#define FILENAMES_STR_LENGTH 10
+#define INDEX_STR_LENGTH 6
+
+int num_of_files = -1;
+
+/* close with error checking */
+int Close(int fd) {
+  int ret = close(fd);
+  if (ret == -1) {
+    perror("close failed");
+    exit(1);
+  }
+  return ret;
+}
+
+/* read with error checking */
+int Read (int fd, void *buf, size_t nbytes) {
+  int ret = read(fd, buf, nbytes);
+  if (ret == -1) {
+    perror("read failed");
+    exit(1);
+  }
+  return ret;
+}
+
+/* write with error checking */
+int Write(int fd, const void *buf, size_t nbytes) {
+  int ret = write(fd, buf, nbytes);
+  if (ret < 0) {
+    perror("write failed");
+    exit(1);
+  }
+  return ret;
+}
+
+/* malloc with error checking */
+void *Malloc (size_t size) {
   void *ret = malloc(size);
   if (ret == NULL) {
     perror("malloc failed");
@@ -16,32 +52,57 @@ void *Malloc (int size) {
   return ret;
 }
 
+/* Returns an empty FreqRecord struct */
+FreqRecord *get_empty_freqrecord () {
+  FreqRecord *empty_freq = Malloc(sizeof(FreqRecord));
+  empty_freq->freq = 0;
+  strcpy(empty_freq->filename, "\0");
+  return empty_freq;
+}
 
-/* Returns an array of FreqRecord structs for the word word.
+/* Returns the number of lines in a file */
+int count_lines (FILE *fp) {
+  int lines = 0;
+  char line[MAXLINE];
+
+  while(fgets(line, MAXLINE, fp) != NULL) {
+    lines++;
+  }
+  return lines;
+}
+
+/*
+* Returns an array of FreqRecord structs for the word word.
+* One FreqRecord for each file where the word is found
 */
 FreqRecord *get_word(char *word, Node *head, char **file_names) {
-  // figure out how many file names there are
-  int number_of_files;
-  for(number_of_files = 0; file_names[number_of_files] != NULL; number_of_files++) {}
+  // figure out how many file names there are only if this has never been calculated before
+  if (num_of_files == -1){
+    num_of_files = 0;
+    while(strlen(file_names[num_of_files]) > 0) {
+      num_of_files++;
+    }
+  }
+  // for(number_of_files = 0; file_names[number_of_files] != NULL; number_of_files++) {}
   // declare an array of structs the same length as the number of file file_names
-  FreqRecord *frequencies = Malloc(number_of_files * sizeof(FreqRecord));
+  FreqRecord *frequencies = Malloc(num_of_files * sizeof(FreqRecord));
   // iterate through the linked list
   Node *cur = head;
 
+  int num_found = 0;
   while(cur) {
-    int i = 0;
-    if (strcmp(cur->word, word) == 0) {
-      for (; i < number_of_files; i++) {
+    if (strcmp(cur->word, word) == 0) { // if the word is found in the index
+      for (int i = 0; i < num_of_files; i++) {
         // if you find the word, copy the frequency at that position to the FreqRecord
         frequencies[i].freq = cur->freq[i];
         strncpy(frequencies[i].filename, file_names[i], PATHLENGTH);
+        num_found++;
       }
     }
     if(cur->next) { //advance to the next item in the linkedlist
       cur = cur->next;
     } else { // no more items, add the empty frequency to show no more data
-      frequencies[i].freq = 0;
-      strcpy(frequencies[i].filename, "\0");
+      frequencies[num_found] = *get_empty_freqrecord();
       cur = NULL;
     }
   }
@@ -53,15 +114,58 @@ FreqRecord *get_word(char *word, Node *head, char **file_names) {
 */
 void print_freq_records(FreqRecord *frp) {
     int i = 0;
-
     while (strlen(frp[i].filename) > 0) {
-        printf("%d    %s\n", frp[i].freq, frp[i].filename);
+        printf("%d\t%s\n", frp[i].freq, frp[i].filename);
         i++;
     }
 }
 
-/* Complete this function for Task 2 including writing a better comment.
+/* Takes the path to a directory that
+* contains the index and filenames files, as well as the file descriptors
+* representing the read end (in) and the write end (out) of the pipe that
+* connects it to the parent.
+* It writes to the out file descriptor one FreqRecord for each file in which the
+* word has a non-zero frequency, followed by an empty FreqRecord to signal the
+* end of data.
 */
 void run_worker(char *dirname, int in, int out) {
-    return;
+  Node *head = NULL;
+  FILE *filenames_fp;
+  FreqRecord *frequencies;
+  char query_word[MAXWORD];
+  char index_file_path[strlen(dirname) + INDEX_STR_LENGTH + 1];
+  char filenames_file_path[strlen(dirname) + FILENAMES_STR_LENGTH + 1];
+  char *filenames[num_of_files];
+
+  strcpy(filenames_file_path, dirname);
+  if ((filenames_fp = fopen(strcat(filenames_file_path, "/filenames"), "r")) == NULL) {
+    perror("fopen for names file");
+    exit(1);
+  }
+  num_of_files = count_lines(filenames_fp);
+  if (fclose(filenames_fp) != 0) {
+    perror("fclose for names file");
+    exit(1);
+  }
+
+  strcpy(index_file_path, dirname);
+  strcat(index_file_path, "/index");
+
+  read_list(index_file_path, filenames_file_path, &head, filenames);
+
+  filenames[num_of_files] = "\0";
+  while(Read(in, query_word, MAXWORD) > 0) {
+    query_word[strlen(query_word) - 1] = '\0'; //remove \n character
+    frequencies = get_word(query_word, head, filenames);
+    for (int i = 0; i < num_of_files; i++) {
+      if(frequencies[i].freq > 0) {
+        // printf("%d\t%s\n", frequencies[i].freq, frequencies[i].filename);
+        Write(out, &(frequencies[i]), sizeof(FreqRecord)); // TODO: weird display of freq when writing stdout
+      }
+    }
+  }
+  Close(in);
+  Write(out, get_empty_freqrecord(), sizeof(FreqRecord));
+  Close(out);
+  return;
 }
