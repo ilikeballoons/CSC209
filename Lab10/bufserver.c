cca658ac4f7fe3788b6cf610cda49979bb16fcfd
Denny Wiseman <d.wiseman@utoronto.ca>
2018-11-21 16:02:35 -0500

    completed bufserver

--- Lab10/bufserver.c
+++ Lab10/bufserver.c
@@ -38,7 +38,9 @@ int main() {
         int nbytes;
         while ((nbytes = read(fd, after, room)) > 0) {
             // Step 1: update inbuf (how many bytes were just added?)
-
+            if (inbuf+nbytes <= room) {
+              inbuf += nbytes;
+            }
 
             int where;
 
@@ -55,24 +57,25 @@ int main() {
                 // using print statement below.
                 // Be sure to put a '\0' in the correct place first;
                 // otherwise you'll get junk in the output.
-
-
+                buf[where-2] = '\0';
                 printf("Next message: %s\n", buf);
                 // Note that we could have also used write to avoid having to
                 // put the '\0' in the buffer. Try using write later!
 
                 // Step 4: update inbuf and remove the full line from the buffer
                 // There might be stuff after the line, so don't just do inbuf = 0.
+                inbuf -= where;
+                memset(buf, '\0', where - 1);
 
                 // You want to move the stuff after the full line to the beginning
                 // of the buffer.  A loop can do it, or you can use memmove.
                 // memmove(destination, source, number_of_bytes)
-
+                memmove(buf, buf + where, inbuf);
 
             }
             // Step 5: update after and room, in preparation for the next read.
-
-
+            after = &(buf[inbuf]);
+            room = BUFSIZE - inbuf;
         }
         close(fd);
     }
@@ -90,5 +93,11 @@ int main() {
  * Definitely do not use strchr or other string functions to search here. (Why not?)
  */
 int find_network_newline(const char *buf, int n) {
+  if (n > BUFSIZE) { return - 1; } // invalid search index
+  for (int i = 0; i < n; i++) {
+    if (buf[i] == '\r' && buf[i+1] == '\n') {
+      return (i+1)+1;
+    }
+  }
     return -1;
 }
